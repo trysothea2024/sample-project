c9534c868f9bdbf82383aeb0995d9cfda0c4a1dd
trysothea2024 <135026984+trysothea2024@users.noreply.github.com>
2023-08-15 00:30:29 +0700

    Add modal when click on each country

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     const [searchQuery, setSearchQuery] = useState('');
     const [sortOrder, setSortOrder] = useState('asc');
     const [currentPage, setCurrentPage] = useState(1);
+    const [selectedCountry, setSelectedCountry] = useState(null);
 
-    const itemsPerPage = 10;
+    const itemsPerPage = 25;
 
     useEffect(() => {
         async function fetchData() {
@@ -51,6 +52,14 @@ function App() {
 
     const totalPages = Math.ceil(sortedCountries.length / itemsPerPage);
 
+    const openModal = country => {
+        setSelectedCountry(country);
+    };
+
+    const closeModal = () => {
+        setSelectedCountry(null);
+    };
+
     return (
         <div className="app">
             <div className="controls">
@@ -66,14 +75,32 @@ function App() {
             </div>
             <div className="grid">
                 {currentCountries.map((country, index) => (
-                    <div className="country-card" key={index}>
-                        <img src={ country.flags.png } alt={`${country.name.common} Flag`} />
-                        <h3>{ country.name.official }</h3>
-                        <p>2 Character Code: { country.cca2 }</p>
-                        <p>3 Character Code: { country.cca3 }</p>
-                        <p>Native Name: { JSON.stringify(country.name.nativeName) }</p>
-                        <p>Alternative Names: { country.altSpellings.join(', ') }</p>
-                        <p>Country Calling Codes: { JSON.stringify(country.idd) }</p>
+                    <div>
+                        <div className="country-card" key={index}  onClick={() => openModal(country)}>
+                            <img src={ country.flags.png } alt={`${country.name.common} Flag`} />
+                            <h3>Country Name: { country.name.official }</h3>
+                            <p>2 Character Code: { country.cca2 }</p>
+                            <p>3 Character Code: { country.cca3 }</p>
+                            <p>Native Name: { JSON.stringify(country.name.nativeName) }</p>
+                            <p>Alternative Names: { country.altSpellings.join(', ') }</p>
+                            <p>Country Calling Codes: { JSON.stringify(country.idd) }</p>
+                        </div>
+                        {selectedCountry && (
+                            <div className="modal">
+                                <div className="modal-content">
+                                    <span className="close" onClick={closeModal}>&times;</span>
+                                    <div>
+                                        <img src={ country.flags.png } alt={`${country.name.common} Flag`} />
+                                        <h3>Country Name: { country.name.official }</h3>
+                                        <p>2 Character Code: { country.cca2 }</p>
+                                        <p>3 Character Code: { country.cca3 }</p>
+                                        <p>Native Name: { JSON.stringify(country.name.nativeName) }</p>
+                                        <p>Alternative Names: { country.altSpellings.join(', ') }</p>
+                                        <p>Country Calling Codes: { JSON.stringify(country.idd) }</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 ))}
             </div>
